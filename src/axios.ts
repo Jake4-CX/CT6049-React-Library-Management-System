b642fd82d27ec52822f64a1fce8ec6724173860a
= <jake44264@hotmail.co.uk>
2023-10-23 23:33:01 +0100

    fix: corrected env import name

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.VITE_API_URL as string;
+const baseUrl = import.meta.env.VITE_API_ENDPOINT as string;
 
 const api = axios.create({
   baseURL: baseUrl,
