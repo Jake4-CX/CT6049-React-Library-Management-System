056af3eecf23ca75952a4cb4077bb27bb3a19b15
= <jake44264@hotmail.co.uk>
2023-11-30 16:32:46 +0000

    feat: monthly fine card update

--- src/components/dashboard/cards/monthlyFineCard.tsx
+++ src/components/dashboard/cards/monthlyFineCard.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getAllUserFinesPaid } from "../../../api/loanFines";
+import { getAllUserFinesPaidBetweenDates } from "../../../api/loanFines";
 
 const MonthlyFineCard: React.FC = () => {
 
@@ -8,17 +8,23 @@ const MonthlyFineCard: React.FC = () => {
     cacheTime: 1000 * 60 * 5, // 5 minutes
     staleTime: 1000 * 60 * 5,
     queryFn: async () => {
-
-      return await ((await getAllUserFinesPaid()).data).loanFines as { id: string, loanId: string, fineAmount: number, paidAt?: Date }[];
+      return await ((await getAllUserFinesPaidBetweenDates(new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString(), new Date().toISOString())).data).loanFines as { id: string, loanId: string, fineAmount: number, paidAt?: Date }[];
     }
   })
 
   return (
     <>
-      <div className="flex flex-row w-[9rem] h-[6rem] p-4 bg-red-500 rounded-lg">
+      <div className="flex flex-row w-[11rem] h-[7rem] px-2 py-3 bg-red-500 rounded-lg">
         <div className="flex flex-col">
-          <h1 className="text-3xl font-bold text-white">£ { userFine.data?.reduce((prev, curr) => curr.fineAmount + prev, 0) }</h1>
-          <p className="text-sm font-light tracking-tight text-white">Monthly Fine</p>
+          <h2 className="text-lg font-semibold tracking-tight text-white">You've paid</h2>
+          {
+            userFine.isLoading ? (
+              <h1 className="text-3xl font-bold text-white">£ 0.00</h1>
+            ) : (
+              <h1 className="text-3xl font-bold text-white">£ {userFine.data?.reduce((prev, curr) => curr.fineAmount ?? 0 + prev, 0).toFixed(2)}</h1>
+            )
+          }
+          <p className="text-sm font-medium tracking-tight text-white">in Fines in the last 30 days</p>
         </div>
       </div>
     </>
